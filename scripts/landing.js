e24e77b9d77701eb0e867c5c3866de107d7748e3
Samantha Harms <pearl@Samantha-Lynn-Harmss-MacBook-Pro.local>
2016-10-14 12:27:29 -0600

    CP-9 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function(){
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points){
 
     var revealPoint = function(index){
             points[index].style.opacity = 1;
@@ -14,4 +14,18 @@ var animatePoints = function(){
 
 };
 
-animatePoints();
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0]; 
+    // Defining  sellingPoints as the first selling-point; I don't know why the variable name is plural if it's one point
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200; 
+    // define scrollDistance as the distance between the sellingPoints area and the top, then subtract the distance from the fold plus 200px into the selling-points area
+    window.addEventListener('scroll', function(event) { // add scroll event listener 
+       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){ 
+           // this callback does something I don't understand, here. I know this OR condition calls animatePoints() with pointsArray passed in. 
+           animatePoints(pointsArray);
+       }
+    });
+}
