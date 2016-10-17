f852e609d6d8de8c78a802f52d16f9f9e975b400
Samantha Harms <pearl@Samantha-Lynn-Harmss-MacBook-Pro.local>
2016-10-17 12:24:50 -0600

    CP-9 done

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point){
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebKitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points){
-
-    var revealPoint = function(index){
-            points[index].style.opacity = 1;
-            points[index].style.transform = "scaleX(1) translateY(0)";
-            points[index].style.msTransform = "scaleX(1) translateY(0)";
-            points[index].style.WebKitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    for (var i = 0; i < points.length; i++)
-        revealPoint(i);
-
+    forEach(points, revealPoint);
+    // pass in points and the revealPoint function. Where is points coming from (the argument)? Is it an array? forEach is like a loop, animating each point with revealPoint() but pieces vex me. 
 };
 
 window.onload = function() {
@@ -23,7 +21,7 @@ window.onload = function() {
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200; 
     // define scrollDistance as the distance between the sellingPoints area and the top, then subtract the distance from the fold plus 200px into the selling-points area
     window.addEventListener('scroll', function(event) { // add scroll event listener 
-       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){ 
+       if (document.body.scrollTop >= scrollDistance){ 
            // this callback does something I don't understand, here. I know this OR condition calls animatePoints() with pointsArray passed in. 
            animatePoints(pointsArray);
        }
